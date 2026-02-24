0e2f14dc8e36c8d37f7db508927bd0ed3c1acf26
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-02-24 12:05:25 +0100

    main ui setup

--- Program.cs
+++ Program.cs
@@ -1,18 +1,66 @@
 ﻿using database;
 
-User? user = UserRepository.GetUserById(4);
-
-if (user == null)
-  return;
-
-Library? library = UserRepository.GetUserLibrary(user);
-
-if (library != null)
+internal class Program
 {
-  foreach (Game game in library.Games)
+  public static void Main(string[] args)
   {
-    Console.WriteLine(
-        $"Game: {game.Id}, {game.Title}, {game.Price}, {game.Rating} {game.PrintGameCategories()}, Studio Behind The Game {game.Studio.Name} Size of Studio: {game.Studio.Size}"
-    );
+    PrintWelcomeScreen();
+    ShowMainMenu();
+  }
+
+  static void ShowMainMenu()
+  {
+    var games = GameRepository.GetAllGames();
+    int selectedIndex = 0; // index to track selected menu item
+    while (true)
+    {
+      Console.Clear();
+      PrintMenu(games, selectedIndex);
+      int result = MenuSelect(games, selectedIndex);
+      if (result == -1) break; // user made selection, exit menu loop
+      selectedIndex = result;
+    }
+    Console.Clear();
+  }
+
+  static void PrintWelcomeScreen()
+  {
+    Console.Clear();
+    Console.WriteLine(@"                                  Welcome to the GameLib v1.0!");
+    Console.WriteLine(@"====================================================================================================");
+    Console.WriteLine("                                    Press any key to start...");
+    Console.ReadKey();
+  }
+
+  /// <summary>
+  /// Loops through menu titles and prints to console
+  /// Adds arrow to item currently selected from menu
+  /// </summary>
+  static void PrintMenu(List<Game> games, int currentIndex)
+  {
+    Console.WriteLine($"Library(1) | Store(2) | Logged in as: xx | Balance: xx");
+    for (int i = 0; i < games.Count; i++)
+    {
+      if (i == currentIndex)
+      {
+        Console.WriteLine($"> {games[i].Title} - {games[i].Price}");
+      }
+      else
+      {
+        Console.WriteLine($"{games[i].Title} - {games[i].Price} "); // spaces to write over previously selected items
+      }
+    }
+    Console.WriteLine("\nUse ^/v arrows to navigate, Enter to select");
+  }
+
+  static int MenuSelect(List<Game> games, int currentIndex)
+  {
+    return Console.ReadKey().Key switch
+    {
+      ConsoleKey.UpArrow => Math.Max(0, currentIndex - 1), // prevent negative index
+      ConsoleKey.DownArrow => Math.Min(games.Count - 1, currentIndex + 1), // prevent index going over lenght of menu
+      ConsoleKey.Enter => -1,
+      _ => currentIndex,
+    };
   }
 }
--- Repository/GameRepository.cs
+++ Repository/GameRepository.cs
@@ -2,4 +2,42 @@
 
 namespace database;
 
-public static class GameRepository { }
+public static class GameRepository
+{
+  static string connectionString = DBConnection.Connection;
+  public static List<Game> GetAllGames()
+  {
+    List<Game> games = new List<Game>();
+    try
+    {
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        connection.Open();
+        const string query = "SELECT * from game";
+        SqlCommand command = new SqlCommand(query, connection);
+        SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+          int id = reader.GetInt32(reader.GetOrdinal("game_id"));
+          string title = reader.GetString(reader.GetOrdinal("title"));
+          decimal price = reader.GetDecimal(reader.GetOrdinal("price"));
+          double rating = reader.GetDouble(reader.GetOrdinal("rating"));
+          Game game = new Game(
+              id,
+              title,
+              price,
+              rating
+           );
+          games.Add(game);
+        }
+        reader.Close();
+      }
+      return games;
+    }
+    catch (SqlException ex)
+    {
+      Console.WriteLine($"SQL Exception Occured: {ex.Message}");
+      return null;
+    }
+  }
+}
